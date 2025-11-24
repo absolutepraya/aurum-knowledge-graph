046a10eca3ba68effee2fd6a6fae50c9f47cf380
Daffa Abhipraya <daffaabhiprayaputra@gmail.com>
2025-11-24 16:43:49 +0700

    feat: add outputFileTracingIncludes for API console to include assets from @img

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	serverExternalPackages: ["@xenova/transformers"],
+	outputFileTracingIncludes: {
+		"/api/console": ["./node_modules/@img/**"],
+	},
 };
 
 export default nextConfig;
